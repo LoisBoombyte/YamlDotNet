cb1b71c45288c5338125507e58ca18e89ea4bb36
Lois <lois.brun@boombytegames.com>
2020-11-25 15:56:21 +0100

    Added a special Naming Convention for our case

---
+++ YamlDotNet/Serialization/NamingConventions/ZulaUnityNamingConvention.cs
@@ -0,0 +1,21 @@
+﻿using YamlDotNet.Serialization.Utilities;
+
+namespace YamlDotNet.Serialization.NamingConventions
+{
+    //So Unity serializes a ScriptableObject with : 
+    //private Type _field;
+    //public Type Field{get ... set ...}
+    // in --->  _field: content
+    //So we needed this special Naming Convention
+    public class ZulaUnityNamingConvention : INamingConvention
+    {
+        private ZulaUnityNamingConvention() { }
+
+        public string Apply(string value)
+        {
+            return "_" + value.ToCamelCase();
+        }
+
+        public static readonly INamingConvention Instance = new ZulaUnityNamingConvention();
+    }
+}
